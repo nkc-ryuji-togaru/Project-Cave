cdcb46a809a2288914f5cae3c8ed9951bafb0ab4
戸軽隆二 <nkc.ryuji.togaru@gmail.com>
2018-09-06 11:37:18 +0900

    データの整理

--- Assets/DangeonCreate/MapInstantiate.cs
+++ Assets/DangeonCreate/MapInstantiate.cs
@@ -5,32 +5,22 @@
  * 概要
  * ２次元配列の情報を元にダンジョンのモデルを生成するクラス
 */
-using System.Collections;
+using System;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class MapInstantiate : MonoBehaviour {
 
-
-    int[,] map = new int[5,5]{
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0}
-    };
+    // マップ自動生成のアルゴリズム
+    RandomMapGenerate randomMapGenerate;
 
 	void Start () {
-        int ylength = map.GetLength(0);
-        int xlength = map.GetLength(1);
+        MapCreate();
+    }
 
-        for(int y = 0; y < ylength; y++){
-            for(int x = 0; x < xlength; x++){
-                Vector3 pos = new Vector3(x, y, 0);
-            }
-        }
-
-
+    void MapCreate(){
+        randomMapGenerate = new RandomMapGenerate();
     }
 	
 }
